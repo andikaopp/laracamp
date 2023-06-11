8061e3d6c668051db78b57d8440af75f577da2de
andikaopp <andikaopp@gmail.com>
2023-06-11 23:44:25 +0700

    update invoice skipped

--- app/Http/Controllers/User/CheckoutController.php
+++ app/Http/Controllers/User/CheckoutController.php
@@ -117,9 +117,4 @@ class CheckoutController extends Controller
     {
         return view('checkout.success');
     }
-
-    public function invoice(Checkout $checkout)
-    {
-        return $checkout;
-    }
 }
